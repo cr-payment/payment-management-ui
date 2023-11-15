e94a66dbf6accce0e91490ae00c2e6c961b3eb48
TheNhatAT <nhat0103st@gmail.com>
2023-11-15 22:16:14 +0700

    update title

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -28,11 +28,11 @@ export function App() {
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - CrPayment"
+        defaultTitle="CrPayment"
         htmlAttributes={{ lang: i18n.language }}
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta name="description" content="CrPayment application" />
       </Helmet>
 
       <Routes>
